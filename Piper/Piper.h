43c8038191af04821826039de45ab8c6c0819a45
bradhilton <brad.hilton.nw@gmail.com>
2016-02-01 11:40:38 -0700

    Initial Commit

---
+++ Piper/Piper.h
@@ -0,0 +1,19 @@
+//
+//  Piper.h
+//  Piper
+//
+//  Created by Bradley Hilton on 2/1/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Piper.
+FOUNDATION_EXPORT double PiperVersionNumber;
+
+//! Project version string for Piper.
+FOUNDATION_EXPORT const unsigned char PiperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Piper/PublicHeader.h>
+
+
